cd90a15af5c5f02b408b84bb41d4f1feba6385c4
G90SG <71718345+G90SG@users.noreply.github.com>
2021-02-22 12:00:27 +0000

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,21 +1,22 @@
-#variables 
+# Creating variables and declaring data types
 balance = 500 
 withdraw = 0
-#request the amount to be withdrawn from the user
-print "How much would you like to withdraw from your account? "
-#convert the variable to an integer
+# Asking the user the amount to be withdrawn from the user
+print "How much would you like to withdraw from your account? £"
+# Obtaing input from the user converting the variable to an integer
 withdraw = gets.to_i
-#calculate the subtraction of the withdrawl from the balance
+# Calculate the subtraction of the withdrawl from the balance
 balance = balance - withdraw
 if balance >= 0 and balance <= 500 then 
-#if the withdrawl doesn't attempt to take more than the balance, it will print a revised balance 
+# Creating IF statement, once the withdrawl is checked to ensure it is not attemping to take more than the original balance, it will print a revised balance 
   puts "Your new balance is £#{balance}."
 
 else 
-#if the withdrawl attempts to tsake more than the balance, the following statement will be printed.
+# If the withdrawl attempts to take more than the balance, the following statement will be printed.
   print "The withdrawl amount must not be more than your current balance. "
 
 end
+
 
 
 
